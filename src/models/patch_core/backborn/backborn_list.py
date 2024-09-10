229ce7456fddcd265994ed811077eed52146af2c
nkoga <naoki.koga@compmind.co.jp>
2024-09-10 11:36:26 +0900

    add onnx extractor(resnet18)

--- src/models/patch_core/backborn/backborn_list.py
+++ src/models/patch_core/backborn/backborn_list.py
@@ -1,9 +1,11 @@
 from .wide_resnet50 import WideResNet50
 from .resnet50 import ResNet50
 from .resnet18 import ResNet18
+from .resnet18_onnx import ResNet18_ONNX
 
 backborn_list = {
     "wide_resnet50": WideResNet50,
     "resnet50": ResNet50,
     "resnet18": ResNet18,
+    "resnet18_onnx": ResNet18_ONNX,
 }
